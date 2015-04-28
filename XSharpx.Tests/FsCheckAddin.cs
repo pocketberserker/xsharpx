f550b9cea7cdbe0d51fbaa4b3844cb86a217ab8d
pocketberserker <pocketberserker@gmail.com>
2015-04-29 00:37:12 +0900

    setup test project

---
+++ XSharpx.Tests/FsCheckAddin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Core.Extensibility;
+using FsCheck.NUnit;
+using FsCheck.NUnit.Addin;
+
+namespace XSharpx.Tests
+{
+    [NUnitAddin(Description = "FsCheck addin")]
+    public class FsCheckAddin : IAddin
+    {
+        public bool Install(IExtensionHost host)
+        {
+            var tcBuilder = new FsCheckTestCaseBuilder();
+            host.GetExtensionPoint("TestCaseBuilders").Install(tcBuilder);
+            return true;
+        }
+    }
+}
